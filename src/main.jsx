aa2d9b3b8c6f8037e49a9a364ae1131f44989c77
Zebby <48347420+TwilightZebby@users.noreply.github.com>
2025-10-04 13:27:43 +0100

    Add social buttons

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,16 @@ import welcomeEditorIconPng from '/welcomeeditoricon.png';
 import homecordIconPng from '/homecordicon.png';
 import drfterxIconPng from '/dr1fterxicon.png';
 import discordIconPng from '/discordicon.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import all the icons in Free Solid, Free Regular, and Brands styles */
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fas, fab);
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -19,6 +29,17 @@ createRoot(document.getElementById('root')).render(
         Welcome to my personal website. Here you can find stuff I am working on, or have worked on. :)
       </p>
 
+      <div className='social-buttons'>
+        <a target='_blank' rel='noopener' href='https://discord.com/users/156482326887530498' className='button' style={{color: "white", backgroundColor: "#5865F2"}}><span className='social-icon'><FontAwesomeIcon icon='fa-brands fa-discord' /></span> twilightzebby</a>
+        <a target='_blank' rel='noopener' href='https://bsky.app/profile/twilightzebby.bsky.social' className='button' style={{color: "white", backgroundColor: "#1083fe"}}><span className='social-icon'><FontAwesomeIcon icon='fa-brands fa-bluesky' /></span> @twilightzebby.bsky.social</a>
+        <a target='_blank' rel='noopener' href='https://twilightzebby.tumblr.com' className='button' style={{color: "white", backgroundColor: "#001935"}}><span className='social-icon'><FontAwesomeIcon icon='fa-brands fa-tumblr' /></span> @twilightzebby</a>
+        <br />
+        <br />
+        <a target='_blank' rel='noopener' href='https://www.twitch.tv/twilightzebby' className='button' style={{color: "white", backgroundColor: "#6441a4"}}><span className='social-icon'><FontAwesomeIcon icon='fa-brands fa-twitch' /></span> @twilightzebby</a>
+        <a target='_blank' rel='noopener' href='https://github.com/TwilightZebby' className='button' style={{color: "white", backgroundColor: "#333333"}}><span className='social-icon'><FontAwesomeIcon icon='fa-brands fa-github' /></span> twilightzebby</a>
+        <a target='_blank' rel='noopener' href='https://ko-fi.com/twilightzebby' className='button' style={{color: "white", backgroundColor: "#72a4f2"}}><span className='social-icon'><FontAwesomeIcon icon='fa-solid fa-mug-hot' /></span> twilightzebby</a>
+      </div>
+      <br />
       <hr />
 
       <h3 className='section-heading'>Discord Apps (formerly "Bots")</h3>
